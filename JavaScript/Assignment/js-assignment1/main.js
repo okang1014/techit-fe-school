b68f8296dbb0b5d471bf5079411846a9a3555c60
okang1014 <okang1014@gmail.com>
2024-09-26 23:55:27 +0900

    js-assignment1 products 항목 선택 시 선택한 상품 리스트에 추가되는 기능. 보완 필요

--- JavaScript/Assignment/js-assignment1/main.js
+++ JavaScript/Assignment/js-assignment1/main.js
@@ -15,16 +15,38 @@ let products = [
   new Product('중뱃살', 1000),
 ]
 
-console.log(products)
-
+//필요 노드 획득
 let listNode = document.getElementById('list');
+let cartList = document.getElementById('cart-list')
+let totalPrice = document.getElementById('total-price');
 
-//products 배열 내의 아이템 개수만큼 html 화면에 출력
+//조건 1 - JS 의 상품정보대로 초기화면 출력
+//products 배열 내의 상품 개수만큼 리스트 크기 조정
+listNode.setAttribute('size', products.length);
+
+//products 배열 내의 상품 html 화면에 출력
 for (let i = 0; i < products.length; i++) {
+  let productInfo = `${products[i].name} - ${products[i].price}`
   let productNode = document.createElement('option');
-  let productTextNode = document.createTextNode(`${products[i].name} - ${products[i].price}`);
+  productNode.setAttribute('id', `product`)
+  productNode.setAttribute('value', productInfo)
+  let productTextNode = document.createTextNode(productInfo);
 
   productNode.appendChild(productTextNode);
-  console.log(productNode)
   listNode.appendChild(productNode);
+
+  //product 항목을 선택 시 선택한 상품 리스트에 추가
+  productNode.addEventListener('click', () => {
+    let productSelected = list.querySelectorAll('option:checked');
+    productSelected.forEach(data => {
+      let cartItem = document.createElement('li');
+      let cartItemText = document.createTextNode(`${data.textContent}`)
+      cartItem.appendChild(cartItemText);
+      cartList.append(cartItem);
+    });
+
+    let cartItem = cartList.querySelectorAll('li')
+    console.log(cartItem)
+  })
 }
+
