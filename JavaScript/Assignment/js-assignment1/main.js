a79ae7d2a36d465c36d414b0c5f3b40d3f97e0eb
okang1014 <okang1014@gmail.com>
2024-09-27 10:55:31 +0900

    js-assignment1 products 항목 선택 시 선택한 항목의 값 배열에 저장, 선택한 상품만 출력되게 만들기 성공력

--- JavaScript/Assignment/js-assignment1/main.js
+++ JavaScript/Assignment/js-assignment1/main.js
@@ -36,53 +36,41 @@ for (let i = 0; i < products.length; i++) {
   listNode.appendChild(productNode);
 }
 
-//선택한 상품 리스트에 추가하는 로직 수정 필요
-
 //product 항목을 선택 시 선택한 상품 리스트에 추가
 let productNode = listNode.querySelectorAll('option');
-//검토가 필요해보임
-// let productSelected = listNode.querySelectorAll('#product:checked');
-
-//클릭한 상품 리스트에 출력, 하지만 계속해서 추가됨, innerHTML 사용하면 해결되려나?
-// productNode.forEach((product) => {
-//   product.addEventListener('click', function () {
-//     console.log('clickevent')
-//     let cartItem = document.createElement('li');
-//     let cartItemText = document.createTextNode(`${product.textContent}`)
-//     cartItem.appendChild(cartItemText);
-//     cartList.append(cartItem);
-//   })
-// })
 
 //클릭 이벤트로 할 경우
 //1. 하나의 아이템 최초로 클릭했을 때는 list Node 에 추가
 //2. 두번째로 클릭했을 때는 listNode 에서 제거
-// 클릭했던 노드를 배열에 넣고, 반환하도록
-// 재차 클릭한 경우, 배열에서 삭제하는 방법?
+// 클릭했던 노드를 배열에 넣고, 반환
+// 재차 클릭한 경우, 배열에서 삭제
+
+let cart = document.getElementById('cart');
 
 let productSelected = [];
 productNode.forEach((product) => {
   product.addEventListener('click', () => {
-    //선택을 하더라도 하나씩만 productSelected 에 추가되도록
+    //productSelected 배열에 product 의 값이 동일한 요소가 없으면 배열에 추가
     if (productSelected.every((value) => value !== product.value)) {
       productSelected.push(product.textContent);
       console.log(productSelected);
     } else {
+      //productSelected 배열에 선택한 상품이 있는 경우, 배열에서 삭제
       let index = productSelected.findIndex((value) => value == product.value);
       productSelected.splice(index, 1);
       console.log(productSelected);
     }
 
-    productSelected.forEach((product) => {
-      let productNode = document.createElement('li');
-      let productTextNode = document.createTextNode(`${product}`);
-      productNode.appendChild(productTextNode);
-
-      cartList.appendChild(productNode);
-    })
+    //선택한 상품이 담긴 배열 cartList 에 출력
+    let items = ''
+    if (productSelected.length === 0) {
+      cartList.innerHTML = '';
+    } else {
+      productSelected.forEach((product) => {
+        cart.setAttribute('style', 'display: block;');
+        items += `<li>${product}</li>`;
+        cartList.innerHTML = items;
+      })
+    }
   })
 })
-
-//checked 노드를 선택하는 경우,
-//checked 된 노드가 포함된 객체를 화면에 출력
-//check 되지 않은 노드는 자동으로 객체에서 삭제, 그러면 
